4196af8c9eceacf0a713c5853c6e771f97cf42d7
Sithmaka Malawige <hmstharunuwan@students.nsbm.ac.lk>
2025-10-15 15:56:06 +0530

    update DTOs

---
+++ DTOs/BookingDtos.cs
@@ -0,0 +1,9 @@
+﻿namespace BusBookingApi.DTOs
+{
+    public class CreateBookingDto
+    {
+        public int TripId { get; set; }
+        public int UserId { get; set; }
+        public List<int> SeatNumbers { get; set; }
+    }
+}
